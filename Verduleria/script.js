1ad6792cab1c75ab079c2b01bf55d5c43517101d
Sebastián Barroso <sebastianBP@ciencias.unam.mx>
2026-04-08 09:48:15 -0600

    Adjustment

--- Verduleria/script.js
+++ Verduleria/script.js
@@ -5,14 +5,15 @@ data.forEach(createBox);
 
 function createBox(item) {
     const box = document.createElement('div');
-    const {img, text} = item;
+    const {image, text} = item;
     box.classList.add('my_box');
     box.classList.add('column');
     box.classList.add('is-one-quarter');
     box.innerHTML = `
-        <img src="${img}" alt="${text}">
+        <img src="${image}" alt="${text}">
         <div class="info">${text}</div>
     `;
 
     main.appendChild(box);
 }
+
